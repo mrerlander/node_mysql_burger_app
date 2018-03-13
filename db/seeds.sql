d817a41255599eb72548201368806b90274aeb6b
Michael@MIKE-LAPTOP <mrerlander@gmail.com>
2018-03-13 14:25:49 -0500

    untracked update

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (burger_name) VALUES ('Bacon Cheeseburger');
+INSERT INTO burgers (burger_name) VALUES ('Double Cheeseburger');
+INSERT INTO burgers (burger_name) VALUES ('Hamburger');
+INSERT INTO burgers (burger_name) VALUES ('Veggie Burger');
+INSERT INTO burgers (burger_name) VALUES ('Triple Cheeseburger');
+INSERT INTO burgers (burger_name) VALUES ('Rodeo Cheeseburger');
